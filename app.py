752c3ee592ad91a669cf0f352bdcddfeb12f768b
Thomas Carver-Smith <carverj18@hotmail.com>
2025-03-13 16:55:24 +0000

    multiple charts

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 st.title('Test McTest!')
-st.write('Kill in Carriage')
+st.write('Kills in Carriage')
 
 # Generate test data
 np.random.seed(42)
@@ -19,10 +19,39 @@ data = pd.DataFrame({
 fig = px.scatter_3d(
     data, x='x', y='y', z='z',
     color='z', size_max=6,
-    title='3D Scatter Plot',
+    title='Carriage 8"',
     labels={'x': 'X Axis', 'y': 'Y Axis', 'z': 'Z Axis'}
 )
 
 # Streamlit app
-st.title("3D Scatter Plot Example")
+st.plotly_chart(fig)
+
+data = pd.DataFrame({
+    'x': np.random.rand(n) * 10,
+    'y': np.random.rand(n) * 10,
+    'z': np.random.rand(n) * 10
+})
+
+# Create 3D scatter plot using plotly express
+fig = px.scatter_3d(
+    data, x='x', y='y', z='z',
+    color='z', size_max=6,
+    title='Carriage 7',
+    labels={'x': 'X Axis', 'y': 'Y Axis', 'z': 'Z Axis'}
+)
+st.plotly_chart(fig)
+
+data = pd.DataFrame({
+    'x': np.random.rand(n) * 10,
+    'y': np.random.rand(n) * 10,
+    'z': np.random.rand(n) * 10
+})
+
+# Create 3D scatter plot using plotly express
+fig = px.scatter_3d(
+    data, x='x', y='y', z='z',
+    color='z', size_max=6,
+    title='Carriage 6',
+    labels={'x': 'X Axis', 'y': 'Y Axis', 'z': 'Z Axis'}
+)
 st.plotly_chart(fig)
